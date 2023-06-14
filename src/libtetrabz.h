6b354356f0031b785484401f6d56c2cac0a29583
Mitsuaki Kawamura <kawamitsuaki@gmail.com>
2023-06-14 17:58:03 +0900

    Make the functions more transportable (e.g., in case of passing a pointer of const memory block). Courtecy of Siqi Wu (RIKEN).

--- src/libtetrabz.h
+++ src/libtetrabz.h
@@ -1,26 +1,26 @@
 #include <complex.h>
 #pragma once
 
-void libtetrabz_occ(     int *ltetra0, double *bvec, int *nb0, int *nge,
-                         double *eig, int *ngw, double *wght0, int *comm);
-void libtetrabz_fermieng(int *ltetra0, double *bvec, int *nb0, int *nge,
-                         double *eig, int *ngw, double *wght0, double *ef,
-                         double *nelec, int *comm);
-void libtetrabz_dos(     int *ltetra0, double *bvec, int *nb0, int *nge,
-                         double *eig, int *ngw, double *wght0, int *ne0,
-                         double *e0, int *comm);
-void libtetrabz_dbldelta(int *ltetra0, double *bvec, int *nb0, int *nge,
-                         double *eig1, double *eig2, int *ngw, double *wght0,
-                         int *comm);
-void libtetrabz_dblstep( int *ltetra0, double *bvec, int *nb0, int *nge,
-                         double *eig1, double *eig2, int *ngw, double *wght0,
-                         int *comm);
-void libtetrabz_polstat( int *ltetra0, double *bvec, int *nb0, int *nge,
-                         double *eig1, double *eig2, int *ngw, double *wght0,
-                         int *comm);
-void libtetrabz_fermigr( int *ltetra0, double *bvec, int *nb0, int *nge,
-                         double *eig1, double *eig2, int *ngw, double *wght0,
-                         int *ne0, double *e0, int *comm);
-void libtetrabz_polcmplx(int *ltetra0, double *bvec, int *nb0, int *nge,
-                         double *eig1, double *eig2, int *ngw, double *wght0,
-                         int *ne0, double complex *e0, int *comm);
+void libtetrabz_occ(     const int *ltetra0, const double *bvec, const int *nb0, const int *nge,
+                         const double *eig, const int *ngw, double *wght0, const int *comm);
+void libtetrabz_fermieng(const int *ltetra0, const double *bvec, const int *nb0, const int *nge,
+                         const double *eig, const int *ngw, double *wght0, double *ef,
+                         const double *nelec, const int *comm);
+void libtetrabz_dos(     const int *ltetra0, const double *bvec, const int *nb0, const int *nge,
+                         const double *eig, const int *ngw, double *wght0, const int *ne0,
+                         const double *e0, const int *comm);
+void libtetrabz_dbldelta(const int *ltetra0, const double *bvec, const int *nb0, const int *nge,
+                         const double *eig1, const double *eig2, const int *ngw, double *wght0,
+                         const int *comm);
+void libtetrabz_dblstep( const int *ltetra0, const double *bvec, const int *nb0, const int *nge,
+                         const double *eig1, const double *eig2, const int *ngw, double *wght0,
+                         const int *comm);
+void libtetrabz_polstat( const int *ltetra0, const double *bvec, const int *nb0, const int *nge,
+                         const double *eig1, const double *eig2, const int *ngw, double *wght0,
+                         const int *comm);
+void libtetrabz_fermigr( const int *ltetra0, const double *bvec, const int *nb0, const int *nge,
+                         const double *eig1, const double *eig2, const int *ngw, double *wght0,
+                         const int *ne0, const double *e0, const int *comm);
+void libtetrabz_polcmplx(const int *ltetra0, const double *bvec, const int *nb0, const int *nge,
+                         const double *eig1, const double *eig2, const int *ngw, double complex *wght0,
+                         const int *ne0, const double complex *e0, const int *comm);
